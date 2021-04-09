c7e72cd6278138f11dfe68bedf309280ac2621a7
ZachChuba <zdc6@njit.edu>
2021-04-09 23:22:16 +0000

    Added input refs to the form

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef } from 'react';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 
 function UserProfileGrid(props) {
@@ -14,33 +14,42 @@ function UserProfileGrid(props) {
 }
 
 function UserProfileForm(props) {
-    const placeholderAlreadySet = false;
+    const nickNameRef = useRef(null);
+    const ageRef = useRef(null);
+    const genderRef = useRef(null);
+    const bioRef = useRef(null);
+    
     return (
         <Form>
             <Form.Row className="justify-content-md-center">
+                <Form.Group as={Col} controlId="googleName">
+                    <Form.Label>Your Name From The Google OAuth</Form.Label>
+                </Form.Group>
+            </Form.Row>
+            <Form.Row className="justify-content-md-center">
                 <Form.Group as={Col} controlId="formName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Nickname" />
+                    <Form.Control ref={nickNameRef} type="text" placeholder="Nickname" />
                 </Form.Group>
             </Form.Row>
             <Form.Row className="justify-content-md-center">
                 <Form.Group as={Col} controlId="formAge">
                   <Form.Label>Age</Form.Label>
-                  <Form.Control type="number" placeholder="18" />
+                  <Form.Control ref={ageRef} type="number" placeholder="18" />
                 </Form.Group>
                 <Form.Group as={Col} controlId="formGender">
                   <Form.Label>Gender</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
+                  <Form.Control ref={genderRef} as="select" defaultValue="Choose...">
                     <option>Male</option>
                     <option>Female.</option>
                     <option>Non-Binary</option>
                   </Form.Control>
                 </Form.Group>
             </Form.Row>
-            <Form.Row>
+            <Form.Row className="justify-content-md-center">
                 <Form.Group as={Col} controlId="formBio">
                     <Form.Label>Bio</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control ref={bioRef} as="textarea" rows={3} />
                 </Form.Group>
             </Form.Row>
         </Form>

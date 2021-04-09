e12ef20d9cfaed5053f9be460357326ddd818449
ZachChuba <zdc6@njit.edu>
2021-04-09 23:14:40 +0000

    Added form format

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -20,7 +20,27 @@ function UserProfileForm(props) {
             <Form.Row className="justify-content-md-center">
                 <Form.Group as={Col} controlId="formName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Your Name" />
+                    <Form.Control type="text" placeholder="Nickname" />
+                </Form.Group>
+            </Form.Row>
+            <Form.Row className="justify-content-md-center">
+                <Form.Group as={Col} controlId="formAge">
+                  <Form.Label>Age</Form.Label>
+                  <Form.Control type="number" placeholder="18" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGender">
+                  <Form.Label>Gender</Form.Label>
+                  <Form.Control as="select" defaultValue="Choose...">
+                    <option>Male</option>
+                    <option>Female.</option>
+                    <option>Non-Binary</option>
+                  </Form.Control>
+                </Form.Group>
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formBio">
+                    <Form.Label>Bio</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
                 </Form.Group>
             </Form.Row>
         </Form>

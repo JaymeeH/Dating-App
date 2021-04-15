51a0a79b0ebcaf402ab2611ceab4c87297d08738
ZachChuba <zdc6@njit.edu>
2021-04-15 01:22:03 +0000

    Added skeleton code for save button and models for db

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, request
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
 
 app = Flask(__name__, static_folder='./build/static')
 
@@ -8,6 +13,18 @@ app = Flask(__name__, static_folder='./build/static')
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
+
+@app.route('/api/v1/user_profile', methods=['GET', 'POST'])
+def user_profile():
+    '''
+    REST api for saving/fetching the user's profile
+    '''
+    if request.method == 'POST':
+        # Write data to DB
+        return
+    else:
+        # Get data from DB
+        return
 
 
 app.run(

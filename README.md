d3c474c25b85f96ddbb986780a9db5a7cb255267
EC2 Default User <ec2-user@ip-172-31-40-227.ec2.internal>
2021-04-16 16:01:39 +0000

    added love calculator api to app.py with blank fields // added lc_test.py to test love calculator functions // updated readme to set up love calculator api

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 1. `npm install`
 2. `pip install -r requirements.txt`
 
+
 ## Setup
 1. Run `echo "DANGEROUSLY_DISABLE_HOST_CHECK=true" > .env.development.local` in the project directory
+2. Go to `https://rapidapi.com/developer/` and make a new developer account
+3. Create an app and generate an application key
+4. Make a .env file and put this in it `export L_C_KEY='YOUR_KEY_HERE'`
 
 ## Run Application
 1. Run command in terminal (in your project directory): `python app.py`
--- app.py
+++ app.py
@@ -1,14 +1,47 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, request, requests
+from flask_socketio import SocketIO
+from dotenv import load_dotenv, find_dotenv
+from flask_cors import CORS
 
+
+
+
+load_dotenv(find_dotenv())
 app = Flask(__name__, static_folder='./build/static')
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    json=json,
+    manage_session=False
+)
+
+#love calculator url and headers
+love_calculator_url = "https://love-calculator.p.rapidapi.com/getPercentage"
+
+headers = {
+    'x-rapidapi-key': os.getenv('L_C_KEY'),
+    'x-rapidapi-host': "love-calculator.p.rapidapi.com"
+    }
 
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
+    
 
+
+@socketio.on('match')
+def match(user1,user2):
+    querystring = {"fname":user1,"sname":user2}
+    response = requests.request("GET", love_calculator_url, headers=headers, params=querystring)
+    print(response.text)
+
+    
+    
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),

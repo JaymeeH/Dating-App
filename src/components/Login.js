41b99f5764853808ce40c4102d55ffb031e4c1a1
Jaymee Hyppolite <ec2-user@ip-172-31-13-127.ec2.internal>
2021-04-17 19:12:54 +0000

    added subdomain check for NJIT students, setting up server side support for saving user login information to database

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,70 @@
-import React from 'react';
+import React,{ useState, useRef, useEffect } from 'react';
 import { GoogleLogin } from 'react-google-login';
 // initiallizing a refresh token
 import { refreshTokenSetup } from '../utils/refreshToken';
 
 //Load the contents of .env variables
 require('dotenv').config()
-
-
 const clientId = process.env.REACT_APP_CLIENT_ID;
-
-
-
 console.log(clientId)
+const LOGIN_URL = '/api/v1/login';
+
+
 
 const Login = () => {
-   const onSuccess = (res) => {
-     console.log('[Login Success] currentUser:', res.profileObj);  
-     alert(
-         `Logged in successfully welcome ${res.profileObj.name} 😍  `
-         );
-   
+    //USE STATES TO CHECK EMAIL ADDRESS
+    const [Email, setEmail] = useState('');
+    const [LogState, setLogState] = useState(false);
+    const [ifNJIT, setIfNJIT] = useState(false);
+    
+    const onSuccess = (res) => {
+     console.log('[Login Success] currentUser:', res.profileObj);
+     setEmail(res.profileObj.email);
+     setLogState(true);
+     checkEmail(res);
+    alert(
+         `Logged in successfully welcome ${res.profileObj.name} `
+     ); 
+    
      // initializing the setup  
-     refreshTokenSetup(res)  
+     refreshTokenSetup(res);  
    };
    
    const onFailure = (res) => {
        console.log('[Login failed] res:', res);
    };
+   
+   const checkEmail = (res) => {
+       const emailCopy = Email;
+       if (emailCopy.includes('@njit.edu')) {
+           setIfNJIT(true);
+           saveLoginData(res.profileObj.name,res.profileObj.givenName,
+            res.profileObj.email,res.profileObj.image_url);
+       }
+      
+       
+   };
+   const saveLoginData = (name,nickname,email,image_url) => {
+       const loginInfo = {
+           email: email,
+           name: name,
+           nickname: nickname,
+           image: image_url,
+       };
+       fetch(LOGIN_URL, {
+           method: 'POST',
+           headers: {
+               'Content-Type': 'application/json',
+           },
+         body: JSON.stringify(loginInfo)  
+       }).then((response)=>response.json()).then((data)=>{
+           console.log(data);
+       });
+   };
+   console.log(Email);
     return (
         <>
+        <div>
         <GoogleLogin
     clientId={clientId}
     buttonText="Log in with Google"
@@ -38,6 +74,8 @@ const Login = () => {
     style={{ marginTop: '100px' }}
     isSignedIN={true}
     />
+    </div>
+   
         
         </>
         

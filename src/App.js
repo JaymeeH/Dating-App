f3e3f277cd6253a2d4b9f99798c0b6f24aca3858
Peter Pasek <ec2-user@ip-172-31-73-134.ec2.internal>
2021-04-20 03:19:26 +0000

    minor prettier/linting

--- src/App.js
+++ src/App.js
@@ -1,106 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import UserProfileGrid from './UserProfile';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import UserProfileGrid from "./UserProfile";
 
 function App() {
   function isLoggedIn() {
     // stub: todo get isLoggedIn from server session
     return true;
   }
-  
-  const [activeProfile,changeProfile] = useState('','','')
+
+  const [activeProfile, changeProfile] = useState("", "", "");
   function getMatch() {
-    console.log("reached getMatch function")
+    console.log("reached getMatch function");
     document.getElementById("profile").style.display = "block";
-    fetch('/api/v1/match', {
-      method: 'POST',
-      headers: {
-        
-      },
+    fetch("/api/v1/match", {
+      method: "POST",
+      headers: {},
     })
-    .then(response => {
-      return response.json();
-    }).then(responseData => {
-      const newList = [...activeProfile];
-      newList[0] = responseData.name;
-      newList[1] = responseData.percentage;
-      newList[2] = responseData.email;
-      changeProfile(newList);
-    })
-    
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const newList = [...activeProfile];
+        newList[0] = responseData.name;
+        newList[1] = responseData.percentage;
+        newList[2] = responseData.email;
+        changeProfile(newList);
+      });
   }
 
-function unMatch(){
+  function unMatch() {
     document.getElementById("profile").style.display = "none";
     console.log("reached unMatch function");
-    fetch('/api/v1/unMatch', {
-      method: 'POST',
-      headers: {
-        
-      },
+    fetch("/api/v1/unMatch", {
+      method: "POST",
+      headers: {},
     })
-    .then(response =>{
-      return response.json();
-    }).then(responseData => {
-    })
-}
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {});
+  }
 
-function expandProfile(){
-  if (document.getElementById("profileViewer").style.display === "none"){
+  function expandProfile() {
+    if (document.getElementById("profileViewer").style.display === "none") {
       document.getElementById("profileViewer").style.display = "block";
-    }
-    else{
+    } else {
       document.getElementById("profileViewer").style.display = "none";
     }
-}
-
+  }
 
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Route
-            exact
-            path="/"
-          >
+          <Route exact path="/">
             <div className="App">
               <Login />
               <Logout />
-              <button type="button" onClick={() => getMatch()}>Match</button>
-                <div className="Profile" id = "profile">
+              <button type="button" onClick={() => getMatch()}>
+                Match
+              </button>
+              <div className="Profile" id="profile">
                 <p>
-                <button type = "button" onClick = {() => expandProfile()}>{activeProfile[0]}</button>
-                <button type = "button" onClick = {() => unMatch()}>Unmatch</button>
-                <br></br>
-                  <div className = "profileViewer" id = "profileViewer">
-                  Percentage compatability: {activeProfile[1]}%
+                  <button type="button" onClick={() => expandProfile()}>
+                    {activeProfile[0]}
+                  </button>
+                  <button type="button" onClick={() => unMatch()}>
+                    Unmatch
+                  </button>
                   <br></br>
+                  <div className="profileViewer" id="profileViewer">
+                    Percentage compatability: {activeProfile[1]}%<br></br>
                   </div>
                 </p>
-                </div>
+              </div>
             </div>
           </Route>
-          <Route
-            path="/profile" >
-            {isLoggedIn() ? 
-              (
-                <UserProfileGrid />
-              ) : (
-                <Redirect to="/" />
-              )
-            }
+          <Route path="/profile">
+            {isLoggedIn() ? <UserProfileGrid /> : <Redirect to="/" />}
           </Route>
         </Switch>
       </div>
     </BrowserRouter>
   );
 }
-
 
 export default App;

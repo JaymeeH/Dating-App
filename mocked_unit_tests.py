776e182dd7db525682ba748385cd49ed1b966f45
Jaymee Hyppolite <ec2-user@ip-172-31-13-127.ec2.internal>
2021-04-28 23:07:47 +0000

    Mocked Unit test for Login

--- mocked_unit_tests.py
+++ mocked_unit_tests.py
@@ -4,13 +4,74 @@ Test cases for database
 import unittest
 from unittest.mock import patch
 import json
-from app import get_profile_from_db
+from app import get_profile_from_db, login
 
 KEY_INPUT = 'input'
 KEY_EXPECTED = 'expected'
 
 
-
+class TryLogin(unittest.TestCase):
+    '''
+    Test case to test the login Functionality from Login.js
+    '''
+    def setUp(self):
+        '''
+        Setup for the test case
+        '''
+        self.success_test_params = [{
+            KEY_INPUT: 'wrong.extension@gmail',
+            KEY_EXPECTED: {
+                'error': False
+            }
+        }, {
+            KEY_INPUT: 'wrong.extension@yahoo',
+            KEY_EXPECTED: {
+                'error': False
+            },
+        }, {
+            KEY_INPUT: 'right.extension@njit',
+            KEY_EXPECTED: {
+                'error': True,
+            }
+        }]
+        self.initial_db_mock = {
+            'exists@email': {
+                'error': False,
+                'email': 'exists@email',
+                'oath_name': 'it exists',
+                'nickname': 'exists',
+                'age': 21,
+                'gender': 'Non-Binary',
+                'bio': '',
+                'image_url': '',
+            }
+        }
+    def mock_out_query(self, email,oath_name):
+        '''
+        Return None if no user exists,
+        '''
+        if email is None:
+            self.initial_db_mock.email = email
+            self.initial_db_mock.oath_name =oath_name
+        else:
+            pass
+    
+    def mock_login(self,oath_name):
+        '''
+        Return true
+        '''
+        return {'success': True}
+        
+    def test_success(self):
+        '''
+        UnMock and run test cases for Login
+        '''
+        print('Test cases for get_profile_from_db')
+        for test_param in self.success_test_params:
+            actual_result = login
+            expected_result = test_param[KEY_EXPECTED]
+            self.assertEqual(len(actual_result), len(expected_result))
+            self.assertDictEqual(actual_result, expected_result)
 class GetProfileTest(unittest.TestCase):
     '''
     Test case for the function get_profile_from_db

0029965313682e5a9611a78810898649a4c70ff1
Kadeem Bobcombe <ec2-user@ip-172-31-40-227.ec2.internal>
2021-05-04 20:33:11 +0000

    trying to get sendbird_chat to cooperate with aftermatch page

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { App as SendbirdApp } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
-import "./src/sb_chat.css"
+import "./sb_chat.css"
 
 
 require('dotenv').config();
 const APP_ID = process.env.SEND_BIRD_ID
 console.log(APP_ID)
 
-const sb = new App.SendBird({appId: APP_ID});
+//const sb = new SendbirdApp.Sendbird({appId: APP_ID});
 
 const USER_ID = 'kjb45@njit.edu'
 
@@ -18,16 +18,16 @@ const CHAT_URL = 'api/v1/chat';
 
 
 const sb_chat = (props) => {
-    sb.connect(USER_ID, function(user, error) {
-    if (error) {
-        // Handle error
-        console.log('sb connect error')
-    }
-    console.log('reached here sb connect')
-    // The user is connected to Sendbird server.
-    });
+    // sb.connect(USER_ID, function(user, error) {
+    // if (error) {
+    //     // Handle error
+    //     console.log('sb connect error')
+    // }
+    // console.log('reached here sb connect')
+    // // The user is connected to Sendbird server.
+    // });
     
-    
+    console.log('reached sb_chat')
     return (
         <div className="sb_chat">
             <h1>SendBird App</h1>
